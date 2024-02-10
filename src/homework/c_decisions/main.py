0d99b93326d480738ed147b8df247eba5e2ea15e
iioveunicorns <157081232+iioveunicorns@users.noreply.github.com>
2024-02-10 07:55:53 +0000

    Homework 2

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,16 @@
 #
+
+from decisions import get_options_ratio, get_faculty_rating
+
+def main():
+    options = float(input("Enter the number of options: "))
+    total = float(input("Enter the total number: "))
+
+    ratio = get_options_ratio(options, total)
+
+    faculty_rating = get_faculty_rating(ratio)
+
+    print("Faculty Rating:", faculty_rating)
+
+if __name__ == "_main_":
+    main()
--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,27 @@
 #
+import unittest
+from src.homework.c_decisions.decisions import get_options_ratio, get_faculty_rating
+
+
+class test_config(unittest.TestCase):
+    
+    def test_get_options_ratio_1(self):
+        self.assertEqual(get_options_ratio(5, 20), 0.25)
+    
+    def test_get_options_ratio_2(self):
+        self.assertEqual(get_options_ratio(10, 20), 0.5)
+
+    def test_get_faculty_rating_1(self):
+        self.assertEqual(get_faculty_rating(0.91), 'Excellent')
+
+    def test_get_faculty_rating_2(self):
+        self.assertEqual(get_faculty_rating(0.85), 'Very Good')
+
+    def test_get_faculty_rating_3(self):
+        self.assertEqual(get_faculty_rating(0.71), 'Good')
+
+    def test_get_faculty_rating_4(self):
+        self.assertEqual(get_faculty_rating(0.66), 'Needs Improvement')
+
+    def test_get_faculty_rating_5(self):
+        self.assertEqual(get_faculty_rating(0.45), 'Unacceptable')
