7f1442a22a5a8ade5b592c96eaa1b74504937a42
iioveunicorns <157081232+iioveunicorns@users.noreply.github.com>
2024-02-15 23:04:47 +0000

    Homework 2

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -2,15 +2,11 @@
 
 from decisions import get_options_ratio, get_faculty_rating
 
-def main():
-    options = float(input("Enter the number of options: "))
-    total = float(input("Enter the total number: "))
+options = float(input("Enter the number of options: "))
+total = float(input("Enter the total number: "))
 
-    ratio = get_options_ratio(options, total)
+ratio = get_options_ratio(options, total)
 
-    faculty_rating = get_faculty_rating(ratio)
+faculty_rating = get_faculty_rating(ratio)
 
-    print("Faculty Rating:", faculty_rating)
-
-if __name__ == "_main_":
-    main()
+print("Faculty Rating:", faculty_rating)
