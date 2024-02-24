ee0183a3dc4d46cd98a9a988f61e00d16f4d27c1
iioveunicorns <157081232+iioveunicorns@users.noreply.github.com>
2024-02-24 22:01:15 +0000

    Homework 4

--- run_tests.py
+++ run_tests.py
@@ -3,10 +3,7 @@ import unittest
 the file in /tests/homework/b_in_proc_out/tests_decisions
 has the test functions
 '''
-from tests.homework.d_repetition import tests_repetition
+from tests.homework.e_functions import tests_functions
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
-
-#from tests.homework.c_decisions import tests_decisions
-
+suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,14 @@
 #
+from value_return import get_hour, get_minutes, get_seconds
+
+def main():
+    epoch_seconds = 3800
+
+    hours = get_hour(epoch_seconds)
+    minutes = get_minutes(epoch_seconds)
+    seconds = get_seconds(epoch_seconds)
+
+    print(f"The time is:  {hours:02d}:{minutes:02d}:{seconds:02d}" )
+
+main()
+
